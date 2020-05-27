7801a48e63b1aa1e56450718efae1c85a694e625
empty-works <pama.mark@gmail.com>
2020-05-27 23:51:53 +0200

    Added while loop so getline reads each line in the file. Added a delimiter for getline to only read the text before the stats value.

--- main.cpp
+++ main.cpp
@@ -4,13 +4,18 @@
 
 int main() {
 	
-	std::string file_name {"stats.txt"}	
+	std::string file_name {"stats.txt"};	
 	std::ifstream in_file;
 	
 	in_file.open(file_name);
 	if(!in_file) {
 		std::cout << "Error opening file." << std::endl;	
 		return 1;
+	}
+
+	while(in_file) {
+		std::string line;
+		std::getline(in_file, line, ":");	
 	}
 
 	in_file.close();	
