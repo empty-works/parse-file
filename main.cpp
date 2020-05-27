a1a6f9fb204b1a3a33b1d6674fe8023bd5b94c3d
empty-works <pama.mark@gmail.com>
2020-05-27 23:41:06 +0200

    Initialized local variables and opened file.

--- main.cpp
+++ main.cpp
@@ -1,7 +1,12 @@
 #include <iostream>
 #include <fstream>
+#include <string>
 
 int main() {
-
+	
+	std::string file_name {"stats.txt"}	
+	std::ifstream in_file;
+	
+	in_file.open(file_name);
 	return 0;
 }
